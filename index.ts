965ba2416a1833d399d340521457a472dbdf0ee4
Tim <tim@gaul.dev>
2021-10-21 21:29:17 +1000

    First change

--- index.ts
+++ index.ts
@@ -19,7 +19,8 @@ async function serveHttp(conn: Deno.Conn) {
     // objects.
     const body = `Your user-agent is:\n\n${requestEvent.request.headers.get(
       "user-agent",
-    ) ?? "Unknown"}`;
+    ) ?? "Unknown"}<br/>
+    Yebo Gogo!`;
     // The requestEvent's `.respondWith()` method is how we send the response
     // back to the client.
     requestEvent.respondWith(
